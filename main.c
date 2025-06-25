122f76219265f89981bdd6b3ad9c66d23bba5235
Tiago Freitas <freitastiago1910@gmail.com>
2025-06-25 15:07:30 -0300

    ADD: inserir_minhoca_no_mapa e imprimir_matriz

--- main.c
+++ main.c
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 #define TAM 10
 #define TAM_MINHOCA 5
-struct Ponto{
+typedef struct{
     int x;
     int y;
     char simbolo;
-};
+} Ponto;
 
 
 typedef struct{
 
-    struct Ponto segmentos[5];
+    Ponto segmentos[5];
 } Minhoca;
 
 int inicializar_matriz(char matriz[TAM][TAM]){
@@ -30,14 +30,30 @@ Minhoca inicializar_minhoca(){
     for (int i = 1; i < TAM_MINHOCA - 1; i++){
         minhoca.segmentos[i].simbolo = '*';
     }
-    minhoca.segmentos[TAM_MINHOCA - 1].simbolo = '•';
+    minhoca.segmentos[TAM_MINHOCA - 1].simbolo = '.';
     return minhoca;
+}
+
+void inserir_minhoca_no_mapa(Minhoca minhoca, char matriz[TAM][TAM], Ponto ponto){
+    for(int i = 0;i<TAM_MINHOCA;i++){
+        matriz[i][ponto.y] = minhoca.segmentos[i].simbolo;
+    }
+}
+
+void imprimir_matriz(char matriz[TAM][TAM]){
+    for(int i = 0; i < TAM; i++){
+        for(int j = 0; j < TAM; j++){
+            printf("%c ", matriz[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 /*
 TODO: 
 Inicializar a matriz
 Criar o vetor(minhoca)
+Inserir a minhoca no mapa (primeiro arbitrariamente)
 Lógica de movimento(A cabeca move primeiro e cada parte segue a anterior)
 Criar obstaculos aleatorios
 JOGADOR ESCOLHE -> Onde a gente começa e QTD movimentos!
@@ -49,8 +65,13 @@ Contar casas que passaram
 */
 
 int main(){
+    Ponto ponto;
+    ponto.x = 0;
+    ponto.y = 0;
     char matriz[TAM][TAM];
     inicializar_matriz(matriz);
     Minhoca minhoca = inicializar_minhoca();
+    inserir_minhoca_no_mapa(minhoca, matriz, ponto);
+    imprimir_matriz(matriz);
     return 0;
 }
